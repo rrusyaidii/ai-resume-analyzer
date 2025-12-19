5fcfa03bd64c4cfd31e45fc6932abe37c619cce4
Rusyaidi <116720602+rrusyaidii@users.noreply.github.com>
2025-12-20 01:53:30 +0800

    next commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -15,10 +15,11 @@ export default function Home() {
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-16">
           <h1>Track Your Applications & Resume Ratings</h1>
           <h2>Review your submissions and check AI-powered feedback</h2>
         </div>
+        
         {resumes.length > 0 && (
           <div className="resumes-section">
             {resumes.map((resume) => (
