8d2a2fe706f1c7b4ce67b7bad4d03940a022817e
Rusyaidi <116720602+rrusyaidii@users.noreply.github.com>
2026-01-27 17:50:36 +0800

    add auth

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -5,16 +5,26 @@ export const meta = () => [
   { name: "description", content: "Log into your account" },
 ];
 
-const auth = () => {
-  const { isLoading } = usePuterStore();
+const Auth = () => {
+  const { isLoading, auth } = usePuterStore();
 
   return (
-    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover min-h-screen flex items-center justify-center">
       <div className="gradient-border shadow-lg">
         <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
           <div className="flex flex-col items-center gap-2 text-center">
             <h1>Welcome</h1>
             <h2>Log In</h2>
+          </div>
+
+          <div>
+            {isLoading ? (
+              <button className="auth-button animate-pulse">
+                <p>Signing you in...</p>
+              </button>
+            ) : (
+              <>{auth.isAuthenticated}</>
+            )}
           </div>
         </section>
       </div>
@@ -22,4 +32,4 @@ const auth = () => {
   );
 };
 
-export default auth;
+export default Auth;
