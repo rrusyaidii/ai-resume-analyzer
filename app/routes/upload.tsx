5dfc1d5fa44eb17b8533cf95d6acb94d540bcd49
Rusyaidi <116720602+rrusyaidii@users.noreply.github.com>
2026-02-02 02:20:18 +0800

    upload file and form submit

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -5,8 +5,24 @@ import Navbar from "~/components/Navbar";
 const upload = () => {
   const [isProcessing, setIsProcessing] = useState(false);
   const [statusText, setStatusText] = useState("");
+  const [file, setFile] = useState<File | null>(null);
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {};
+  const handleFileSelect = (file: File | null) => {
+    setFile(file);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget.closest("form");
+
+    if (!form) return;
+
+    const formData = new FormData(form);
+
+    const companyName = formData.get("company-name");
+    const jobTitle = formData.get("job-title");
+    const jobDescription = formData.get("job-description");
+  };
 
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
@@ -62,7 +78,7 @@ const upload = () => {
 
               <div className="form-div">
                 <label htmlFor="uploader">Upload Resume</label>
-                <FileUploader />
+                <FileUploader onFileSelect={handleFileSelect} />
               </div>
 
               <button className="primary-button" type="submit">
